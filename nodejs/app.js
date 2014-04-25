936bd7dd566eaaa4ed75f56df50d24fbdc151540
lukaslehmann <lukas92lehmann@gmail.com>
2014-04-25 09:09:41 +0200

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -9,7 +9,7 @@ function rabbitUrl() {
     for (var label in svcInfo) {
       var svcs = svcInfo[label];
       for (var index in svcs) {
-        var uri = svcs[index].credentials.uri;
+        var uri = svcs[index].credentials.url;
         if (uri.lastIndexOf("amqp", 0) == 0) {
           return uri;
         }
